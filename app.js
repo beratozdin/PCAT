b6b0881f646d8fa4fe369cea8b952c5d59044127
beratozdin <berat.ozdin2016@gtu.edu.tr>
2021-05-26 23:26:51 +0300

    style is updated

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path=require('path');
+const ejs=require('ejs');
 const app = express();
 
+//Template engine
+app.set("view engine", "ejs");
+
+//Middlewares
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
+  res.render('index');
+})
 
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/add', (req, res) => {
+  res.render('add');
 })
 
 const port = 3000;
