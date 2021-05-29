82a1baee98edb09df341bff7bb420216fe223269
beratozdin <berat.ozdin2016@gtu.edu.tr>
2021-05-29 19:02:57 +0300

    Adding photo to mongodb database is completed

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 const express = require('express');
 const path=require('path');
 const ejs=require('ejs');
+const Photo = require('./models/Photo');
+const mongoose=require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
 
 //Template engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
-})
+
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+
+  res.render('index', {
+    photos
+  })
+});
 
 app.get('/about', (req, res) => {
   res.render('about');
-})
+});
 
 app.get('/add', (req, res) => {
   res.render('add');
-})
+});
+
+app.post('/photos', async (req, res) => {
+  
+  await Photo.create(req.body)
+  res.redirect('/')
+});
 
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
